bdb581af0ce18d17776493e73d1ee61130fdcd9b
SureshU <sureshdevops@gmail.com>
2025-09-25 18:50:29 +0530

    message

---
+++ 4_redis.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[32m"
+N="\e[0m"
+user=$(id -u)
+file="/var/log/roboshoplog/"
+name=$(echo $0 | cut -d "." -f1)
+log=$file/$name.log
+
+mkdir -p $file
+
+
+if [ $user -ne 0 ];then
+    {
+        echo -e "$R Please take root permission $N" | tee -a $log
+        exit 1
+    }         
+fi
+
+validate()
+{
+    if [ $? -ne 0 ];then
+        echo -e "$2 $R is FAILURE $N"
+        else
+        echo -e "$2 $G is SUCCESS $N"
+    fi 
+}
+
+dnf list installed redis &>>$log
+validate $? "Redis Available"
+
+dnf module disable redis -y
+validate $? "disabling default version"
+
+dnf module enable redis:7 -y
+validate $? "enabling version"
+
+
+systemctl enable redis &>>$log
+validate $? "Redis Enabling"
+
+systemctl start redis &>>$log
+validate $? "Redis starting"
+
+# sed -i -e 's/127.0.0.1/0.0.0.0 -e /protected-mode/c protected-mode no'/etc/redis/redis.conf &>>$log
+sed -i 's/127.0.0.1/0.0.0.0' /etc/redis/redis.conf &>>$log
+validate $? "Redis Enabling Public Access"
+
+sed -i '/protected-mode/c protected-mode no' /etc/redis/redis.conf &>>$log
+validate $? "Protected Mode off"
+
+systemctl restart redis &>>$log
+validate $? "restarting service"
+
+
+        
+  
+
+
+
+
+
+
