6ff5d75336f8f7981017c7a8b52e95e6a7cfde9a
SureshU <sureshdevops@gmail.com>
2025-09-25 13:23:08 +0530

    message

---
+++ ec2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ami="ami-09c813fb71547fc4f"
+sg="sg-07c8acf3fa6b923fa" # replace with your SG ID
+zone="Z0948150OFPSYTNVYZOY" # replace with your ID
+dns="sureshdevops.fun"
+
+for instance in $@ # mongodb redis mysql
+do
+    instance_id=$(aws ec2 run-instances --image-id $ami --instance-type t3.micro --security-group-ids $sg --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+    # Get Private IP
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$dns" # mongodb.daws86s.fun
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$dns" # daws86s.fun
+    fi
+
+    echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $zone \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
+done
